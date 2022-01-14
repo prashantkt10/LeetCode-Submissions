108ddcf0d28b3e300232390ba20ded8ea0a52d52
Prashant Tripathi <pkt10dec@gmail.com>
2022-01-14 10:20:18 +0530

    Time: 270 ms (6.67%), Space: 45.1 MB (5.02%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i=0, last;
+    while(i<nums.length) {
+        let c=nums[i];
+        if(last==c) {
+            nums.splice(i,1);
+            continue;
+        }
+        last=c;
+        i++;
+    }
+    console.log(nums);
+    return nums.length;
+};
