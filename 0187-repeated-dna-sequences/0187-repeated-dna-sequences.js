65889e75da8c2ebe97a7f959f53aa7b68ac9a6a7
Prashant Tripathi <pkt10dec@gmail.com>
2022-11-14 17:38:11 +0530

    Time: 228 ms (23.45%), Space: 70.3 MB (7.30%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.js
@@ -6,21 +6,22 @@
 
 var findRepeatedDnaSequences = function(s) {
     let patterns={};
+    //n
+    //n2
     for(let i=0; i<s.length; i++) {
-        let pattern=s.slice(i,10+i);
-        console.log(i,pattern)
-        if(pattern.length<10) break;
-        if(patterns[pattern]) {
-            patterns[pattern]+=1;
+        let pattern=s.slice(i,10+i); //n
+        if(pattern.length<10) break; //1
+        if(patterns[pattern]) { //1
+            patterns[pattern]+=1; //1
             continue;
         }
-        patterns[pattern]=1;
+        patterns[pattern]=1;//1
     }
-    let allPatterns=Object.keys(patterns);
-    for(let i=0; i< allPatterns.length; i++) {
-        if(patterns[allPatterns[i]]<2) delete patterns[allPatterns[i]];
+    let allPatterns=Object.keys(patterns);//n
+    for(let i=0; i< allPatterns.length; i++) {//n
+        if(patterns[allPatterns[i]]<2) delete patterns[allPatterns[i]];//1
     }
-    return Object.keys(patterns);
+    return Object.keys(patterns);//n
 };
     
 // abcd
