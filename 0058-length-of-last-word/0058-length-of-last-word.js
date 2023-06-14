844c3ea75fc75cf65203dd94f9aeeda34e04ad5d
Prashant Tripathi <prashantkt10@gmail.com>
2023-06-14 08:56:32 +0530

    Time: 54 ms (78.31%), Space: 41.8 MB (68.05%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,8 +2,21 @@
  * @param {string} s
  * @return {number}
  */
+// Solution -1
+// var lengthOfLastWord = function(s) {
+//     s=s.trim();
+//     s=s.split(" ");
+//     return s[s.length-1].length;
+// };
+
+// Solution - 2
 var lengthOfLastWord = function(s) {
     s=s.trim();
-    s=s.split(" ");
-    return s[s.length-1].length;
+    let len=0;
+    for(let i=s.length-1; i>=0; i--) {
+        if(s[i]==' ') return len;
+        len+=1;
+    }
+    return len;
 };
+
